acdf28e7fde0b64d9729143d0d1fd03dce05e638
Somanumte <felixjosemartinezbrito@gmail.com>
2026-01-02 17:56:54 -0400

    Proyecto Luxera, V.1.0

--- app/__init__.py
+++ app/__init__.py
@@ -51,7 +51,10 @@ def create_app(config_name='development'):
     from app.routes.main import main_bp
     from app.routes.inventory import inventory_bp
     from app.routes.api.catalog_api import catalog_api_bp
+    from app.routes.api.customers import customers_bp
 
+
+    app.register_blueprint(customers_bp)
     app.register_blueprint(auth_bp)
     app.register_blueprint(main_bp)
     app.register_blueprint(inventory_bp)
